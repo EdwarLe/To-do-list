2ea36d7c6cde7798ce451c359563e0495cc6d5f6
Edwar León <edwar.leon.m@gmail.com>
2022-07-21 19:40:40 -0500

    Clase 07 completada

--- script.js
+++ script.js
@@ -1,42 +1,5 @@
-import checkComplete from "./componentes/checkComplete.js";
-import deleteIcon from "./componentes/deleteIcon.js";
+import { addTask } from "./componentes/addTask.js";
 
 const btn = document.querySelector("[data-form-btn]");
-
-const addTask = (evento) => {
-    const list = document.querySelector("[data-list]");
-    const task = createTask(evento);
-    list.appendChild(task);
-
-}
-
-const createTask = (evento) => {
-    evento.preventDefault();
-    const input = document.querySelector("[data-form-input]");
-    const calendar = document.querySelector("[data-form-date]");
-    const value = input.value;
-    const date = calendar.value;
-    const dateFormat = moment(date).format("DD/MM/YYYY");
-    console.log(dateFormat);
-    const task = document.createElement("li");
-    task.classList.add("card");
-    input.value = "";
-
-    const taskContent = document.createElement("div");
-    const titleTask = document.createElement("span");
-    titleTask.classList.add("task");
-    titleTask.innerHTML = value;
-    taskContent.appendChild(checkComplete());
-    taskContent.appendChild(titleTask);
-    //taskContent.appendChild(deleteIcon());
-    const dateElement = document.createElement("span");
-    dateElement.innerHTML = dateFormat;
-
-    //task.innerHTML = content;
-    task.appendChild(taskContent);
-    task.appendChild(dateElement);
-    task.appendChild(deleteIcon());
-    return task;
-};
 
 btn.addEventListener("click", addTask);
