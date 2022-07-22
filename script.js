57eb6b37afe4578d505a8f3bb0c446678c31bb8c
Edwar León <edwar.leon.m@gmail.com>
2022-07-21 20:21:56 -0500

    clase 07 completada

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 import { addTask } from "./componentes/addTask.js";
-
+import { readTasks } from "./componentes/readTasks.js";
 const btn = document.querySelector("[data-form-btn]");
 
 btn.addEventListener("click", addTask);
+
+readTasks();
